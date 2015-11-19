45ac83f534170527ea3ac76a7ba9bd738952aff6
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-19 17:07:24 -0500

    Part 1A: Created Main shell

---
+++ Part_1/A/Main.java
@@ -0,0 +1,17 @@
+//Andriy Zasypkin and Jason Tufano
+//2015-11-19
+//Final Project part 1A
+
+/** Description:
+  *   Write a program in java to ...
+  *
+  * Assumptions:
+  *   - 
+  */
+
+public class Main {
+  public static void main(String[] args) {
+    
+    System.exit(0);
+  }
+}
