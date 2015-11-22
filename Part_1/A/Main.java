a57c638f445c2d697ee2e2659ba976801ad1bbb2
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-21 22:18:42 -0500

    Fixed stuff, finished assignment
    A partial merge to master will be needed

--- Part_1/A/Main.java
+++ Part_1/A/Main.java
@@ -3,15 +3,42 @@
 //Final Project part 1A
 
 /** Description:
-  *   Write a program in java to ...
+  *   provide a bunch of output for a calc
   *
   * Assumptions:
   *   - 
   */
 
+import java.io.*;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    //create an input reader object
+    BufferedReader input
+            = new BufferedReader(new InputStreamReader(System.in));
     
+    Matrix A = new Matrix();
+    A.input(input);
+    
+    Matrix B = new Matrix();
+    for(int i=0; i<1; i++) {
+      B.input(input);
+      String a = A.toString();
+      String b = B.toString();
+      System.out.println("===============");
+      System.out.printf("|x1|   %s^-1   %s\n",
+        a.split("\n")[0], b.split("\n")[0]);
+      System.out.printf("|x2| = %s    * %s\n",
+        a.split("\n")[1], b.split("\n")[1]);
+      System.out.printf("|x3|   %s      %s\n",
+        a.split("\n")[2], b.split("\n")[2]);
+      System.out.println("===============");
+      
+      A.inverse().multiply(B).print();
+    }
+    
+    //close stream
+    input.close();
     System.exit(0);
   }
 }
--- Part_1/A/Matrix.java
+++ Part_1/A/Matrix.java
@@ -2,6 +2,8 @@
 //2015-11-19
 //Final Project part 1A
 
+import java.io.*;
+
 public class Matrix {
   private double data[][];
   private int    rows;
@@ -39,42 +41,107 @@ public class Matrix {
     for(int i=0; i<this.rows-1; i++) {
       for(int j=0; j<this.cols-1; j++) {
         A.set(i, j, this.get(i<r ? i : i+1, j<c ? j : j+1));
+        //No need for output
       }
     }
+    return A;
   }
 
   //return deturminant of this matrix
   public double det() {
+    System.out.println("\nDet:");
+    double d = det(0, "", 1);
+    
+    System.out.printf("Det = %8.2f\n", d);
+    return d;
+  }
+  
+  private double det(int indent, String prefix, double mult) {
     if(this.rows != this.cols)
       throw new RuntimeException("Illegal matrix dimensions");
-    if(this.rows == 0)
+    if(this.rows == 0) {
+      for(int i=0; i<indent; i++) {
+        System.out.print("  ");
+      }
+      System.out.printf("det( [] ) = 1\n");
       return 1;
-    if(this.rows == 1)
+    }
+    if(this.rows == 1) {
+      for(int i=0; i<indent; i++) {
+        System.out.print("  ");
+      }
+      System.out.printf("det( [%8.2f] ) = %8.2f\n",
+        this.get(0, 0), this.get(0, 0));
       return this.get(0, 0);
-    if(this.rows == 2)
+    }
+    if(this.rows == 2) {
+      for(int i=0; i<indent; i++) {
+        System.out.print("  ");
+      }
+      System.out.printf("det( %c ) = %s((%8.2f * %8.2f) - (%8.2f * %8.2f)) = %8.2f\n", 
+                 indent+'A',
+                 prefix,
+                 this.get(0, 0), this.get(1, 1),
+                 this.get(1, 0), this.get(0, 1),
+                 (this.get(0, 0)*this.get(1, 1)-this.get(1, 0)*this.get(0, 1))*mult);
       return this.get(0, 0)*this.get(1, 1) - this.get(1, 0)*this.get(0, 1);
+    }
 
     double sum = 0;
     for(int i=0; i<this.rows; i++) {
-      for(int j=0; j<this.cols; j++) {
-        sum += ((i+j)%2 == 0 ? 1 : -1) * this.get(i, j) * this.remove(i, j).det();
+      double a = ((i)%2 == 0 ? 1 : -1);
+      double b = this.get(i, 0);
+      for(int k=0; k<indent; k++) {
+        System.out.print("  ");
       }
+      System.out.printf("%c[%02d][%02d] = %s(-1^(%d))*(%8.2f*det( %c )) = ?\n",
+                        indent+'A', i+1, 1, prefix,
+                        i+2, b, indent+'B');
+      double c = this.remove(i, 0).det(indent+1, "", 1);
+      for(int k=0; k<indent+1; k++) {
+        System.out.print("  ");
+      }
+      System.out.printf("? = %8.2f\n",
+                        a*b*c*mult);
+      sum += a*b*c;
     }
+    return sum;
   }
 
   //returns the transpose of the cofactor(adjoint)
   public Matrix adj() {
+    System.out.println("\nAdj: cofactor(A)^T");
     return this.cofactor().transpose();
+  }
+
+  public void input(BufferedReader input) throws IOException {
+    System.out.print("Rows in matrix A: ");//get rows
+    int rows = Integer.valueOf(input.readLine()).intValue();
+    System.out.print("Columns in matrix A: ");//get columns
+    int cols = Integer.valueOf(input.readLine()).intValue();
+    this.setSize(rows, cols);//and create empty
+    //fill
+    for(int i=0; i<rows; i++) {
+      for(int j=0; j<cols; j++) {
+        System.out.printf("A[%02d][%02d] = ", i+1, j+1);
+        this.set(i, j, Double.valueOf(input.readLine()).doubleValue());
+      }
+    }
   }
 
   //return cofactor of this matrix
   public Matrix cofactor() {
+    System.out.println("\nCofactor:");
     if(this.rows != this.cols)
       throw new RuntimeException("Illegal matrix dimensions");
     Matrix A = new Matrix(this.rows, this.cols);
     for (int i=0; i<this.rows; i++) {
       for (int j=0; j<this.cols; j++) {
-        A.set(j, i, ((i+j)%2 == 0 ? 1 : -1) * this.remove(i, j).det());
+        
+        double d = this.remove(i, j).det(1,
+                                         String.format("(-1^(%d))*", i+j+2),
+                                         (i+j)%2 == 0 ? 1 : -1);
+        A.set(j, i, ((i+j)%2 == 0 ? 1 : -1) * d);
       }
     }
     return A;
@@ -84,17 +151,24 @@ public class Matrix {
   public Matrix inverse() {
     if(this.rows != this.cols) //A^-1 = A.transpose()*(A*A^T)^-1
       return this.transpose().multiply(this.multiply(this.transpose()).inverse());
-    return this.adj()/this.det();;
+    
+    Matrix tmp         = this.adj();
+    double deturminant = this.det();
+    System.out.println("\nInverse: adj(A)/det(A):");
+    tmp.print("/"+deturminant);
+    
+    return tmp.divide(deturminant);
   }
 
   //return the matrix of minors
   public Matrix minors() {
+    System.out.println("\nMinors:");
     if(this.rows != this.cols)
       throw new RuntimeException("Illegal matrix dimensions");
     Matrix A = new Matrix(this.rows, this.cols);
     for(int i=0; i<this.rows; i++) {
       for(int j=0; j<this.cols; j++) {
-        A.set(i, j, this.remove(i, j).det());
+        A.set(i, j, this.remove(i, j).det(1, "", 1));
       }
     }
     return A;
@@ -130,7 +204,7 @@ public class Matrix {
     Matrix A = new Matrix(this.cols, this.rows);
     for(int i=0; i<this.rows; i++)
       for(int j=0; j<this.cols; j++)
-        A.set(j, i, this.get(i, j);
+        A.set(j, i, this.get(i, j));
     return A;
   }
 
@@ -147,14 +221,21 @@ public class Matrix {
 
   //multiply this by A(matrix)
   public Matrix multiply(Matrix A) {
+    System.out.println("\nMultiply by matrix:");
     //After reading the problem statement I realized that this was unnecessary
-    if(this.rows != A.getColumns())
+    if(this.cols != A.getRows())
       throw new RuntimeException("Illegal matrix dimensions");
     Matrix B = new Matrix(this.rows, A.getColumns());
     for(int i=0; i<B.getRows(); i++)
-      for(int j=0; j<B.getColumns(); j++)
-        for(int k=0; k<this.cols; k++)
-          B.set(i, j, B.get(i, j) + this.get(i, k)*A.get(k, j));
+      for(int j=0; j<B.getColumns(); j++) {
+        for(int k=0; k<this.cols; k++) {
+          System.out.printf("  C[%02d][%02d] = %8.2f + %8.2f *%8.2f = %8.2f\n",
+            i+1, j+1, B.get(i, j), this.get(i, k), A.get(k, j),
+            B.get(i, j) + this.get(k, i)*A.get(k, j));
+          B.set(i, j, B.get(i, j) + this.get(k, i)*A.get(k, j));
+        }
+        System.out.println();
+      }
     return B;
   }
 
@@ -167,14 +248,37 @@ public class Matrix {
     return A;
   }
 
+  //divide this by k(scalar)
+  public Matrix divide(double k) {
+    Matrix A = new Matrix(this.rows, this.cols);
+    for(int i=0; i<this.rows; i++)
+      for(int j=0; j<this.cols; j++)
+        A.set(i, j, this.get(i, j) / k);
+    return A;
+  }
+
   //OUTPUT
   // print the matrix
   public void print() {
+    System.out.print(this.toString());
+  }
+  
+  public void print(String sufix) {
+    System.out.print(this.toString(sufix));
+  }
+  
+  public String toString() {
+    return toString("");
+  }
+  
+  public String toString(String sufix) {
+    String out = "";
     for(int i=0; i<this.rows; i++) {
-      System.out.print("  |");
+      out += "|";
       for(int j=0; j<this.cols; j++)
-        System.out.printf("%7.2f ", this.get(i, j));
-      System.out.println("|");
+        out += String.format("%7.2f%s ", this.get(i, j),  sufix);
+      out += "|\n";
     }
+    return out;
   }
 }
