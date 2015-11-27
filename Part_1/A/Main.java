1ae557c5b06b5ccd6456bc6e78af215e2c4fcd8d
Jason W. Tufano <jason.tufano@gmail.com>
2015-11-27 14:30:20 -0500

    Added problem description and assumptions
    Typed up the description verbatim from the worksheet, and made some
    assumptions to clarify the assignment.

--- Part_1/A/Main.java
+++ Part_1/A/Main.java
@@ -2,11 +2,26 @@
 //2015-11-19
 //Final Project part 1A
 
+//TO-DO: Edit Anwari grammar (and possible spelling mistakes)
 /** Description:
-  *   Write a program in java to ...
+  *   Write a program to read two 3x3 matrices, compute the sum, the product of the two matrices.
+  *   Then, compute the transpose, cofactor matrix and the determinant of the resultant matrix. Then
+  *   find the inverse of the first matrix then multiply it by the first column of the second matrix
+  *   to get the third matrix. Also tested using the matrices given in class.
+  *   
+  *   In this program write a different method to perfrom the operations and use proper notation to
+  *   pass the arguments and to access the elements.
+  *   
+  *   Use files for input and output. Make sure to use class and objects. Use constructor and overload
+  *   constructor to initialize the object.
   *
   * Assumptions:
-  *   - 
+  *   - The matrices will be multiplied in the order they are read
+  *   - Only the product matrix will have its transpose, cofactor, and determinant taken.
+  *   - The "first matrix" will be the first matrix read, and the "second matrix" will be the second
+  *   	matrix read.
+  *   - The input and output files will have the same format
+  *   - The files will not be formatted, they will only be formatted when they are printed
   */
 
 public class Main {
