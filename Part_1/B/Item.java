d5b13eb9ee7e0b7d463f6125310eaa34bd6e4eec
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-28 08:19:14 -0500

    Part 1B: fixed auto generated typo

---
+++ Part_1/B/Item.java
@@ -0,0 +1,9 @@
+//Andriy Zasypkin
+//2015-11-28
+//Final Project part 1B
+
+public class Item {
+  public Item() {
+    
+  }
+}
--- Part_1/B/Robot.java
+++ Part_1/B/Robot.java
@@ -1,6 +1,6 @@
 //Andriy Zasypkin and Jason Tufano
 //2015-11-27
-//B
+//Final Project part 1B
 
 public class Robot {
   public Robot() {
