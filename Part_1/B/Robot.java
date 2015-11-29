8b36cdf24957bd9f7070d06cf2e9a6e5f3dce688
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-29 10:54:17 -0500

    Part 1B: Robot can do things
    - move to a station
    - put item down(onto a sation)
    - pick item up(from station)
    Note: the robots methods return false
          if it counld not complete action

--- Part_1/B/Robot.java
+++ Part_1/B/Robot.java
@@ -3,9 +3,35 @@
 //Final Project part 1B
 
 public class Robot {
-  private ItemList items;
-  
+  private Item item;
+  private Station  currentStation;
+
   public Robot() {
-    items = new ItemList();
+    this.items          = null;
+    this.currentStation = null;
+  }
+
+  public void moveToStation(Station station) {
+    this.currentStation = station;
+  }
+
+  public boolean pickItem() {
+    if(this.currentStation == null || this.item != null) {
+      return false;
+    }
+    return (this.item = this.currentStation.getItem()) != null;
+  }
+
+  public boolean putItem() {
+    if(this.currentStation == null || this.item == null) {
+      return false;
+    }
+    if(currentStation.maxedOut()) {
+      return false;
+    }
+  }
+
+  public Item getItem() {
+    return this.item;
   }
 }
