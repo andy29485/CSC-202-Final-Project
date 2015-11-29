59dcb29abbce76ef13d3889123a6f4b99dd0891d
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-28 22:56:05 -0500

    added the methods that are needed

--- Part_1/B/Item.java
+++ Part_1/B/Item.java
@@ -22,66 +22,8 @@ public class Item {
     this.mass = 0;
   }
 
-  //adds item pos right after current item
-  // return false if it could not
-  public boolean insert(Item next) {
-    return this.insert(0, next);
-  }
-
-  //adds item pos positions after current item
-  //returns false if could not add
-  public boolean insert(int pos, Item next) {
-    if(next == null || pos < 0)
-      return false;
-    if(pos == 0) {
-      next.setNext(this.getNext());
-      this.next = next;
-      return true;
-    }
-    if(this.getNext() == null)
-      return false;
-    return this.getNext().insert(pos-1, next);
-  }
-
-  //Adds item to end of the list
-  public void append(Item next) {
-    if(this.getNext() == null)
-      this.setNext(next);
-    else
-      this.getNext().append(next);
-  }
-
-  //removes and returns item from the end of the list
-  public Item pop() {
-    if(this.getNext() == null) {
-      Item next = this.getNext();
-      this.setNext(null);
-      return next;
-    }
-    else {
-      return this.getNext().pop();
-    }
-  }
-
-  //returns number of items ahead of current +1(for this item)
-  public int size() {
-    if(this.getNext() == null)
-      return 1;
-    else
-      return 1 + this.getNext().size();
-  }
-
-  /*Deamed not possible - not double linked - cannot access previouse element
-   * //removes current item(and returns itself)
-   * public Item remove() {
-   *   //TODO
-   *   return this;
-   * }
-   */
-
   //Accessor/Mutator methods
-  private void setNext(Item next) {
-    //private to decrease errors - use append or insert methods
+  public void setNext(Item next) {
     this.next = next;
   }
 
