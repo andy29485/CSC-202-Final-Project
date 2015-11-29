d31acc187745bc74df77d1bbcec36a640dbdadb2
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-11-29 16:18:40 -0500

    Part 1B: the robot "moves"

--- Part_1/B/Robot.java
+++ Part_1/B/Robot.java
@@ -7,11 +7,76 @@ public class Robot {
   private Station  currentStation;
 
   public Robot() {
-    this.items          = null;
+    this.item           = null;
     this.currentStation = null;
   }
 
+  //This method would have been only one line long if not
+  //  for the moving/printing the movements part
   public void moveToStation(Station station) {
+    int current_num;
+    int end_num;
+    if(currentStation.getName().equalsIgnoreCase("Refrigeration Container"))
+      current_num = -2;
+    else if(currentStation.getName().equalsIgnoreCase("Pickup"))
+      current_num = -1;
+    else {
+      String num = currentStation.getName().replaceAll("[^0-9]", "");
+      current_num = Integer.valueOf(num).intValue();
+    }
+
+    if(station.getName().equalsIgnoreCase("Refrigeration Container"))
+      end_num = -2;
+    else if(station.getName().equalsIgnoreCase("Pickup"))
+      end_num = -1;
+    else {
+      String num = station.getName().replaceAll("[^0-9]", "");
+      end_num = Integer.valueOf(num).intValue();
+    }
+
+    if(current_num%2==0 && current_num >= 0 && current_num != 8)
+      System.out.println("Robot moving Left");
+    else if((current_num%2==1 && current_num > 0) || current_num == 8)
+      System.out.println("Robot moving Right");
+    else if(current_num == -2) {
+      System.out.println("Robot moving Backward");
+      current_num = 6;
+    }
+    else if(current_num == -1)
+      System.out.println("Robot moving Forward");
+
+    if(current_num%2 == 1)
+      current_num -= 1;
+    if(current_num == 8)
+      current_num = -1;
+
+    int end_num_b = end_num;
+    if(end_num_b == 8)
+      end_num_b = -1;
+    if(end_num_b == -2)
+      end_num_b = 6;
+    end_num_b = (end_num_b%2==0 && end_num_b>0 )? end_num_b : end_num_b-1;
+
+    while(current_num > end_num_b) {
+      System.out.println("Robot moving Backward");
+      current_num--;
+    }
+    while(current_num < end_num_b) {
+      System.out.println("Robot moving Forward");
+      current_num++;
+    }
+
+    if(end_num%2==0 && end_num >= 0 && end_num != 8)
+      System.out.println("Robot moving Right");
+    else if((end_num%2==1 && end_num > 0) || end_num == 8)
+      System.out.println("Robot moving Left");
+    else if(end_num == -2) {
+      System.out.println("Robot moving Forward");
+    }
+    else if(end_num == -1)
+      System.out.println("Robot moving Backward");
+
+    //The only statment needed to actually move the robot
     this.currentStation = station;
   }
 
