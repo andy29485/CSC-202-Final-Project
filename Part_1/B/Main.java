c639c5cee655d70ad65da77a502c6e2266817ab8
jwtufano <jason.tufano@gmail.com>
2015-11-29 18:39:35 -0500

    Something stupid status: implemented
    idk I’m tired of describing things
    user input, for loops, possibly something that works…

--- Part_1/B/Main.java
+++ Part_1/B/Main.java
@@ -72,13 +72,24 @@ public class Main {
 
     while((item_info = input.readLine()) != null) {
       Item item = new Item();
-      item.setID(Integer.valueOf(item_info));
+      item.setID(Integer.valueOf(item_info).intValue());
       storeRoom.addItem(item);
     }
 
     while(storeRoom.canStart()) {
-      storeRoom.start();
-      //TODO figure out how unload works into this
+    	System.out.println("\nHow many items until unload?");
+    	String s1 = input.readLine();
+    	int numItems = Integer.valueOf(s1).intValue();
+    	for(int i=0; i<numItems; i++){
+    		if(storeRoom.canStart())
+    			storeRoom.start();
+    		else
+    			break;
+    	}
+    	System.out.println("\nUnload now?");
+    	s1 = input.readLine();
+    	if(s1.equalsIgnoreCase("yes") || s1.equalsIgnoreCase("y"))
+    		storeRoom.unload();	
     }
 
     //Closes stream
